6985698d755d2a03c6378e35f0e3d151d1fc0662
Martin Znamenáček <znamema1@users.noreply.github.com>
2017-05-05 12:57:31 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read -r INPUT; do
-    echo "$INPUT" | sed -e 's/([[:lower:]])|([[:upper:]])/\U\1\L\2/g'
+    echo "$INPUT" | sed -E 's/([[:lower:]])|([[:upper:]])/\U\1\L\2/g'
 done
